ed6606d91cfdac5326fc56c90476a7a6b5662c69
rougeayrn <97561459+rougeayrn@users.noreply.github.com>
2025-05-30 14:50:32 -0400

    Fix Source Keeper Avoidance for Center Rooms (#58)
    * Update sourceKeepers.ts
    
    - Added center room check for source keeper avoidance.
    
    * Removed First Fix Attempt
    
    * Update selectors.ts
    
    * Correctly Removed First Attempt...

--- src/lib/WorldMap/selectors.ts
+++ src/lib/WorldMap/selectors.ts
@@ -13,8 +13,7 @@ export const isSourceKeeperRoom = (roomName: string) => {
   if (!parsed) throw new Error('Invalid room name');
   let fmod = Number(parsed[1]) % 10;
   let smod = Number(parsed[2]) % 10;
-  // return !(fmod === 5 && smod === 5) && (fmod >= 4 && fmod <= 6) && (smod >= 4 && smod <= 6);
-  return fmod >= 4 && fmod <= 6 && smod >= 4 && smod <= 6;
+  return !(fmod === 5 && smod === 5) && (fmod >= 4 && fmod <= 6) && (smod >= 4 && smod <= 6);
 };
 
 /**
